4323d8b2c241db080e4893792fc7b174729b1990
Sparrow0005 <cadeoliver111@gmail.com>
2025-10-15 19:29:54 -0400

    Made an EXE

--- logs.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-:: Display the logs for the bot
-pm2 logs discord-bot
-
-:: Prevent the cmd window from closing
-pause
--- restart.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-yt-dlp -U
-pm2 restart all --update-env
